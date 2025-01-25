469955ced6b2327329715b4b03497dd1bce4a815
Julian Santiago Diaz <107156927+juliansdiaz@users.noreply.github.com>
2025-01-25 00:24:24 -0500

    Programmed player and camera movement

---
+++ Assets/Scripts/PlayerCameraLook.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerCameraLook : MonoBehaviour
+{
+    //Variables
+    [SerializeField] float mouseSensitivity = 350.0f;
+    [SerializeField] float xRotation = 0f;
+
+    public Transform playerBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Lock cursor in game mode
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        //Get mouse movement input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotate camera vertically
+        xRotation -= mouseY;
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        //Set vertical rotation limits
+        xRotation = Mathf.Clamp(xRotation, -20f, 62f);
+
+        //Move camera horizontally
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
